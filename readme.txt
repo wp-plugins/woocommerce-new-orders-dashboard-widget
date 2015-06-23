5c3d3bfa20b68eceb01091c91a9006e0464a1b02
swadeshswain <swadeshswain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 08:54:23 +0000

    New Upload
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-new-orders-dashboard-widget/trunk@1186048 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== woocommerce New Orders Dashboard Widget===
+Contributors: swadeshswain
+Tags: woocommerce, New Orders, Dashboard Widget,Order,status .
+Requires at least: 3.0
+Tested up to: 4.2
+Stable tag: 1.0
+License: GPLv2 or later
+
+woocommerce New Order's Dashboard Widget Shows the recent new orders from the user On the Dashboard .
+
+== Description ==
+
+woocommerce New Order's Dashboard Widget Shows the recent new orders from the user On the Dashboard  panel,
+also it shows the Order's status.
+
+
+== Installation ==
+
+1. Upload woocommerce New Orders Dashboard Widget to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Check your Dashboard ,Enjoy.
+
+
+
+
+
+== Screenshots ==
+
+1. Woocommerce New Order's Dashboard Widget 1
+
+2. Woocommerce New Order's Dashboard Widget 2
+
+== Changelog ==
+= 1.0 =
+* woocommerce New Order's Dashboard Widget Shows the recent new orders from the user On the Dashboard .
+
+
---
+++ woocommerce-new-orders-dashboard.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Plugin Name: woocommerce New Orders Dashboard Widget
+Plugin URI: http://odrasoft.com/
+Description: woocommerce New Orders Dashboard Widget Shows the recent new orders from the user On the Dashboard .
+Version: 0.1
+Author: swadeshswain
+Author URI: http://odrasoft.com/
+License: GPLv2 or later
+*/
+add_action('wp_dashboard_setup', 'od_woo_dashboard_widgets');
+function od_woo_dashboard_widgets() {
+global $wp_meta_boxes;
+wp_add_dashboard_widget('od_woo_widget', 'WooCommerce Recent Orders', 'od_dashboard_woo');
+}
+function od_dashboard_woo() {
+global $wpdb;
+global $woocommerce ;
+
+$customer_orders = get_posts( apply_filters( 'woocommerce_my_account_my_orders_query', array(
+	'numberposts' => $order_count,
+	'meta_key'    => '_customer_user',
+	'meta_value'  => get_current_user_id(),
+	'post_type'   => 'shop_order',
+	'post_status' => 'publish'
+	
+) ) );
+
+if ( $customer_orders ) : ?>
+	<table class="shop_table my_account_orders" width="100%">
+
+		<thead>
+			<tr>
+				<th class="order-number"><span class="nobr"><?php _e( 'Order Id', 'woocommerce' ); ?></span></th>
+				<th class="order-date"><span class="nobr"><?php _e( 'Date', 'woocommerce' ); ?></span></th>
+				<th class="order-status"><span class="nobr"><?php _e( 'Status', 'woocommerce' ); ?></span></th>
+				<th class="order-total"><span class="nobr"><?php _e( 'Total', 'woocommerce' ); ?></span></th>
+				<th class="order-actions"><span class="nobr"><?php _e( 'Action', 'woocommerce' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+		<?php
+			foreach ( $customer_orders as $customer_order ) {
+				$order = new WC_Order();
+
+				$order->populate( $customer_order );
+
+				$status     = get_term_by( 'slug', $order->status, 'shop_order_status' );
+				$item_count = $order->get_item_count();
+
+				?><tr class="order">
+					<td class="order-number">
+						<a href="<?php echo $order->get_view_order_url(); ?>">
+							<?php echo $order->get_order_number(); ?>
+						</a>
+					</td>
+					<td class="order-date">
+						<time datetime="<?php echo date( 'Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
+					</td>
+					<td class="order-status" style="text-align:left; white-space:nowrap;">
+                   <?php $ostatus = $order->status ;if($ostatus == "on-hold"){?>
+                    
+						<span style="color:#FF0000"><?php echo ucfirst( __( $order->status, 'woocommerce' ) ); ?></span>
+                        
+                        <?php } ?>
+                        <?php $ostatus = $order->status ;if($ostatus == "processing"){?>
+                    
+						<span style="color:#F8BD27"><?php echo ucfirst( __( $order->status, 'woocommerce' ) ); ?></span>
+                        
+                        <?php } ?>
+                        
+                        <?php $ostatus = $order->status ;if($ostatus == "completed"){?>
+                    
+						<span style="color:#0F9D58"><?php echo ucfirst( __( $order->status, 'woocommerce' ) ); ?></span>
+                        
+                        <?php } ?>
+                        
+					</td>
+					<td class="order-total">
+						<?php echo sprintf( _n( '%s for %s item', '%s for %s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count ); ?>
+					</td>
+					<td class="order-actions">
+						<?php
+							$actions = array();
+
+							if ( in_array( $order->status, apply_filters( 'woocommerce_valid_order_statuses_for_payment', array( 'pending', 'failed' ), $order ) ) ) {
+								$actions['pay'] = array(
+									'url'  => $order->get_checkout_payment_url(),
+									'name' => __( 'Pay', 'woocommerce' )
+								);
+							}
+
+							if ( in_array( $order->status, apply_filters( 'woocommerce_valid_order_statuses_for_cancel', array( 'pending', 'failed' ), $order ) ) ) {
+								$actions['cancel'] = array(
+									'url'  => $order->get_cancel_order_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ),
+									'name' => __( 'Cancel', 'woocommerce' )
+								);
+							}
+
+							$actions['view'] = array(
+								'url'  => $order->get_view_order_url(),
+								'name' => __( 'View', 'woocommerce' )
+							);
+							
+							$actions = apply_filters( 'woocommerce_my_account_my_orders_actions', $actions, $order );
+
+							if ($actions) {
+								foreach ( $actions as $key => $action ) {
+									echo '<a href="' . esc_url( $action['url'] ) . '" class="button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+								}
+							}
+						?>
+					</td>
+				</tr><?php
+			}
+		?></tbody>
+
+	</table>
+<?php endif; ?>
+
+<?php 
+}?>
